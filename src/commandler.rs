046e96064bf0443cc5de47bb55148f31895f50af
Ben Stidham <ben.stidham1@gmail.com>
2025-06-30 15:50:45 -0500

    removed warnings

--- src/commandler.rs
+++ src/commandler.rs
@@ -1,18 +1,22 @@
 // Get functions from local files
 
-#[path = "alias.rs"] mod alias;
-#[path = "get_info.rs"] mod get_info;
-#[path = "prompt.rs"] mod prompt;
-#[path = "get_path.rs"] mod get_path;
+#[path = "alias.rs"]
+mod alias;
+#[path = "get_info.rs"]
+mod get_info;
+#[path = "get_path.rs"]
+mod get_path;
+#[path = "prompt.rs"]
+mod prompt;
 
 // Imports
 use std::env::set_current_dir;
-use std::process::{Command, exit};
 use std::path::Path;
+use std::process::{exit, Command};
 
 // Handle commands
 #[allow(dead_code)]
-pub fn commandler(line_read: String){
+pub fn commandler(line_read: String) {
     // Swap out home key (~), back key (..), and here key (.)
     let line_swapped = get_path::here_swap(get_path::back_swap(get_path::home_swap(line_read)));
     // Get paths
@@ -22,7 +26,7 @@ pub fn commandler(line_read: String){
     let command = parts.next().unwrap();
     let args = parts;
     // Match case for first part of command
-    match command{
+    match command {
         // Exit when `exit` command typed
         "exit" => {
             exit(0);
@@ -47,18 +51,15 @@ pub fn commandler(line_read: String){
         // Else, try to run command on system
         _ => {
             // Check command existence
-            let command_tuple = check_command_existence(command.clone(), paths.clone());
+            let command_tuple = check_command_existence(command, paths.clone());
             // If command exists, run it
-            if command_tuple.0{
+            if command_tuple.0 {
                 let full_path_command = command_tuple.1;
-                let mut child = Command::new(full_path_command)
-                    .args(args)
-                    .spawn()
-                    .unwrap();
+                let mut child = Command::new(full_path_command).args(args).spawn().unwrap();
                 let _result = child.wait().unwrap();
             }
             // Else, pass to non-existence handler
-            else{
+            else {
                 command_not_exist(command);
             }
         }
@@ -66,27 +67,26 @@ pub fn commandler(line_read: String){
 }
 
 // Check if command exists
-fn check_command_existence(command: &str, paths: Vec<String>) -> (bool, String){
+fn check_command_existence(command: &str, paths: Vec<String>) -> (bool, String) {
     // Initialize a bool + String tuple to return
     let mut return_tuple = (false, "".to_owned());
     // Set return tuple to absolute path if one is given; ./file works too
-    if command.chars().nth(0).unwrap() == '/'{
+    if command.chars().nth(0).unwrap() == '/' {
         return_tuple = (true, command.to_owned());
-    }
-    else{
+    } else {
         // Run through path vector
-        for single_path in paths{
-            if Path::new(format!("{}/{}", single_path.clone(), command.clone()).as_str()).exists(){
+        for single_path in paths {
+            if Path::new(format!("{}/{}", single_path.clone(), command).as_str()).exists() {
                 return_tuple = (true, format!("{}/{}", single_path, command));
             }
         }
     }
     // Return the return tuple
-    return return_tuple
+    return return_tuple;
 }
 
 // Handle command non-existence
-fn command_not_exist(command: &str){
+fn command_not_exist(command: &str) {
     // Just print out that it couldn't be run
     println!("command {} could not be run", command);
 }
